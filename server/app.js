143d4684348a15ff6d8540701de33c9a5aed424f
nipanimaju <zuc.pawel@gmail.com>
2020-08-21 20:01:21 +0200

    moze teraz

--- server/app.js
+++ server/app.js
@@ -33,5 +33,7 @@ mongoose.connect(`mongodb+srv://testconnect:testconnect@cluster0.suzcz.mongodb.n
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/todos", todosRouter);
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 module.exports = app;
