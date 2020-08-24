667298e0387ac6d5a0af18816cd81ad9b0cc2d73
nipanimaju <zuc.pawel@gmail.com>
2020-08-24 10:37:43 +0200

    heroku

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -7,7 +7,7 @@ import Stats from "./Stats"
 import Footer from "./Footer"
 import axios from "axios";
 
-const link =  `http://localhost:3001/todos`;
+const link =  `/todos`;
 // class component
 class TodoContainer extends React.Component {
 
