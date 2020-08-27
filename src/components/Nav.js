c4008c6bfdc79bf4e3cc92630706891e665d25ba
nipanimaju <zuc.pawel@gmail.com>
2020-08-27 10:47:22 +0200

    styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import  { Link } from "react-router-dom"
+import "./Nav.scss"
 
-const styleUL = {
-    listStyle: "none",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    width: "50vw",
-    height: "5vh",
-    marginBottom: "20px",
-    background: "#038203"
-    
-}
-const styleLi = {
-    display: "inline",
-    marginRight: "20px",
-}
-const styleA = {
-    textDecoration: "none",
-    color: "white"
-}
 
 const Nav = () => {
   return (
     <nav>
-        <ul style={styleUL}>
-            <Link style={styleA} to='/'><li style={styleLi}>Home</li></Link> 
-            <Link style={styleA} to='/impressum'><li style={styleLi}>Impressum</li></Link>
-            <Link style={styleA} to='/help'><li style={styleLi}>Help</li></Link>
+        <ul className="styleUL">
+            <Link className="styleA" to='/'><li className="styleLi">Home</li></Link> 
+            <Link className="styleA" to='/impressum'><li className="styleLi">Impressum</li></Link>
+            <Link className="styleA" to='/help'><li className="styleLi">Help</li></Link>
         </ul>
     </nav>
   );
