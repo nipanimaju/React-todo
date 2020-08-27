2ce2e1b2e5b1deb28c69ebefb9ef11229df9731f
nipanimaju <zuc.pawel@gmail.com>
2020-08-27 13:49:07 +0200

    detail

--- src/components/Impressum/Impressum.js
+++ src/components/Impressum/Impressum.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./Impressum.scss"
 
 
 const Impressum = () => {
   return (
-    <div className="impressum" >
+    <div className="imp" >
       <h1>Impressum</h1>
       <p>
+      Contact me: zuc.pawel@gmail.com <br /> <br />
       Copyright © 2020 Pawel Zuchowski
         </p>
     </div>
--- src/components/TodoItem/ToDoDetail.js
+++ src/components/TodoItem/ToDoDetail.js
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import "./ToDodetail.scss"
 
 const ToDoDetail = (props) => {
     const [task, setTask] = useState("");
-  useEffect(() => {
-    getData();
-    
-  }, []);
-    const getData = async () => { 
+    useEffect(() => {
+        getData();
+
+    }, []);
+    const getData = async () => {
         let response = await fetch(`/todos/${props.match.params.id}`);
         let data = await response.json()
         setTask(data);
-      }
-  return ( 
-   
-    <div >
-      <h1>Task Details</h1>
-      <p>
-       title: {task.title}
-        </p>
-    </div>
-  
-  );
+    }
+    return (
+
+        <div className="detail">
+            <h1>Task: {task.title}</h1>
+            <div className="main">
+                <div className="wrapper2">
+                    <h2>Comment:</h2>
+                    <textarea rows="10">
+                        something here
+                    </textarea>
+                </div>
+                <div className="wrapper">
+                    <div className="start"> <p>START: 00:00:00</p> </div>
+                    <div className="end"> <p>END: 00:00:00</p> </div>
+                    <div className="duration"> <p>TIME: 00:00:00</p> </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default ToDoDetail;
